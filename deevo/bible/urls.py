12f3992da25d682405fc1802897689fbc974d7e2
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-09 22:23:12 +0800

    Add ability to read different versions of the Bible

--- deevo/bible/urls.py
+++ deevo/bible/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'bible'
 urlpatterns = [
     url(r'^$', views.BookView.as_view(), name='book'),
-    url(r'^(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.chapter, name='chapter'),
-    url(r'^(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/(?P<verse_id>[0-9]+)/$', views.verse, name='verse')
+    url(r'^(?P<version_id>[a-z]+)/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.chapter, name='chapter'),
+    url(r'^(?P<version_id>[a-z]+)/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/(?P<verse_id>[0-9]+)/$', views.verse, name='verse')
 ]
--- deevo/bible/views.py
+++ deevo/bible/views.py
@@ -1,7 +1,8 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.views import generic
 
-from .models import KeyEnglish, TBbe
+from .models import *
 
 class BookView(generic.ListView):
     template_name = 'bible/index.html'
@@ -18,20 +19,49 @@ class ChapterView(generic.DetailView):
         """
         return TBbe.objects.filter(b = book_id, c = chapter_id)
 
-def chapter(request, book_id, chapter_id):
-    verse_list = get_list_or_404(TBbe, b=book_id, c=chapter_id)
+def chapter(request, version_id, book_id, chapter_id):
+    if version_id == 'asv':
+        model = TAsv
+    elif version_id == 'bbe':
+        model = TBbe
+    elif version_id == 'darby':
+        model = TDby
+    elif version_id == 'kjv':
+        model = TKjv
+    elif version_id == 'wbt':
+        model = TWbt
+    else:
+        raise Http404("Bible version does not exist")
+    version = get_object_or_404(BibleVersionKey, abbreviation=version_id.upper())
+    verse_list = get_list_or_404(model, b=book_id, c=chapter_id)
     book = get_object_or_404(KeyEnglish, pk=book_id)
     context = {
+        'version': version,
+        'version_link': version_id,
         'verse_list': verse_list,
         'book': book,
         'chapter': chapter_id,
     }
     return render(request, 'bible/chapter.html', context)
 
-def verse(request, book_id, chapter_id, verse_id):
-    verse = get_object_or_404(TBbe, b=book_id, c=chapter_id, v=verse_id)
+def verse(request, version_id, book_id, chapter_id, verse_id):
+    if version_id == 'asv':
+        model = TAsv
+    elif version_id == 'bbe':
+        model = TBbe
+    elif version_id == 'darby':
+        model = TDby
+    elif version_id == 'kjv':
+        model = TKjv
+    elif version_id == 'wbt':
+        model = TWbt
+    else:
+        raise Http404("Bible version does not exist")
+    version = get_object_or_404(BibleVersionKey, abbreviation=version_id.upper())
+    verse = get_object_or_404(model, b=book_id, c=chapter_id, v=verse_id)
     book = get_object_or_404(KeyEnglish, pk=book_id)
     context = {
+        'version': version,
         'verse': verse,
         'book': book,
         'chapter': chapter_id,
