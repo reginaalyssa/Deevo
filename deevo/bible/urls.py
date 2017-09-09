41faaea13a383cee4cf37ba192d38f29c4e16793
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-09 21:06:49 +0800

    Display verses given a book and chapter of Bible

--- deevo/bible/urls.py
+++ deevo/bible/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'bible'
 urlpatterns = [
     url(r'^$', views.BookView.as_view(), name='book'),
+    url(r'^(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.chapter, name='chapter'),
 ]
--- deevo/bible/views.py
+++ deevo/bible/views.py
@@ -1,7 +1,29 @@
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.views import generic
 
-from .models import KeyEnglish
+from .models import KeyEnglish, TBbe
 
 class BookView(generic.ListView):
     template_name = 'bible/index.html'
     model = KeyEnglish
+
+class ChapterView(generic.DetailView):
+    template_name = 'bible/chapter.html'
+    model = TBbe
+    context_object_name = 'verse_list'
+
+    def get_queryset(self):
+        """
+        Get all verses in given chapter.
+        """
+        return TBbe.objects.filter(b = book_id, c = chapter_id)
+
+def chapter(request, book_id, chapter_id):
+    verse_list = get_list_or_404(TBbe, b=book_id, c=chapter_id)
+    book = get_object_or_404(KeyEnglish, pk=book_id)
+    context = {
+        'verse_list': verse_list,
+        'book': book,
+        'chapter': chapter_id,
+    }
+    return render(request, 'bible/chapter.html', context)
