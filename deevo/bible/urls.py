6e2adda1c71eba52cbe4437fde6ef8f55a44dffe
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-09 21:52:02 +0800

    Add page for viewing verse and add link to verse in chapter page

--- deevo/bible/urls.py
+++ deevo/bible/urls.py
@@ -6,4 +6,5 @@ app_name = 'bible'
 urlpatterns = [
     url(r'^$', views.BookView.as_view(), name='book'),
     url(r'^(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.chapter, name='chapter'),
+    url(r'^(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/(?P<verse_id>[0-9]+)/$', views.verse, name='verse')
 ]
--- deevo/bible/views.py
+++ deevo/bible/views.py
@@ -27,3 +27,13 @@ def chapter(request, book_id, chapter_id):
         'chapter': chapter_id,
     }
     return render(request, 'bible/chapter.html', context)
+
+def verse(request, book_id, chapter_id, verse_id):
+    verse = get_object_or_404(TBbe, b=book_id, c=chapter_id, v=verse_id)
+    book = get_object_or_404(KeyEnglish, pk=book_id)
+    context = {
+        'verse': verse,
+        'book': book,
+        'chapter': chapter_id,
+    }
+    return render(request, 'bible/verse.html', context)
