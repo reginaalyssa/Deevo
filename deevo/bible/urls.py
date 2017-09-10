99995490439b36a412e9db24a86bfadcbe3a5aa6
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-11 06:38:37 +0800

    Add change version to each verse page

--- deevo/bible/urls.py
+++ deevo/bible/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'bible'
 urlpatterns = [
-    url(r'^change_version/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.change_version, name='change_version'),
+    url(r'^change_version/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.change_version_chapter, name='change_version_chapter'),
+    url(r'^change_version/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/(?P<verse_id>[0-9]+)/$', views.change_version_verse, name='change_version_verse'),
     url(r'^(?P<version_id>[a-z]+)/$', views.BookView.as_view(), name='book'),
     url(r'^(?P<version_id>[a-z]+)/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/$', views.chapter, name='chapter'),
     url(r'^(?P<version_id>[a-z]+)/(?P<book_id>[0-9]+)/(?P<chapter_id>[0-9]+)/(?P<verse_id>[0-9]+)/$', views.verse, name='verse')
--- deevo/bible/views.py
+++ deevo/bible/views.py
@@ -26,13 +26,21 @@ class ChapterView(generic.DetailView):
         """
         return TBbe.objects.filter(b = book_id, c = chapter_id)
 
-def change_version(request, book_id, chapter_id):
+def change_version_chapter(request, book_id, chapter_id):
     if request.method == 'POST':
         form = VersionForm(request.POST)
 
         if form.is_valid():
             version = form.cleaned_data['version']
             return HttpResponseRedirect(reverse('bible:chapter', args=(version.abbreviation.lower(), book_id, chapter_id,)))
+
+def change_version_verse(request, book_id, chapter_id, verse_id):
+    if request.method == 'POST':
+        form = VersionForm(request.POST)
+
+        if form.is_valid():
+            version = form.cleaned_data['version']
+            return HttpResponseRedirect(reverse('bible:verse', args=(version.abbreviation.lower(), book_id, chapter_id, verse_id,)))
 
 def chapter(request, version_id, book_id, chapter_id):
     if version_id == 'asv':
@@ -83,10 +91,12 @@ def verse(request, version_id, book_id, chapter_id, verse_id):
     version = get_object_or_404(BibleVersionKey, abbreviation=version_id.upper())
     verse = get_object_or_404(model, b=book_id, c=chapter_id, v=verse_id)
     book = get_object_or_404(KeyEnglish, pk=book_id)
+    form = VersionForm(request.POST or None, initial={"version": version.id})
     context = {
         'version': version,
         'verse': verse,
         'book': book,
         'chapter': chapter_id,
+        'form': form,
     }
     return render(request, 'bible/verse.html', context)
