94ff32a539f43cbb0e3cd593397ce35d68134091
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-12 04:56:17 +0800

    Add list view page for devotions

--- deevo/devotions/urls.py
+++ deevo/devotions/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from .forms import ChooseVerseForm, ReflectionForm
 from .views import NewDevotionWizard
+from . import views
 
 app_name = 'devotions'
 urlpatterns = [
-    url(r'^new/$', NewDevotionWizard.as_view([ChooseVerseForm, ReflectionForm]))
+    url(r'^new/$', NewDevotionWizard.as_view([ChooseVerseForm, ReflectionForm]), name='new'),
+    url(r'^view/$', views.DevotionView.as_view(), name='view_devotions'),
 ]
--- deevo/devotions/views.py
+++ deevo/devotions/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+from django.views import generic
 
 from formtools.wizard.views import SessionWizardView
 
@@ -14,7 +16,7 @@ FORMS = [('choose_verse', ChooseVerseForm),
 TEMPLATES = {'0': 'devotions/choose.html',
              '1': 'devotions/reflect.html'}
 
-class NewDevotionWizard(SessionWizardView):
+class NewDevotionWizard(LoginRequiredMixin, SessionWizardView):
     def get_template_names(self):
         return [TEMPLATES[self.steps.current]]
 
@@ -63,4 +65,45 @@ class NewDevotionWizard(SessionWizardView):
             new_context['verse'] = verse
         if self.steps.current == '1':
             context.update(new_context)
+        return context
+
+class DevotionView(LoginRequiredMixin, generic.ListView):
+    template_name = 'devotions/view.html'
+    context_object_name = 'latest_devotion_list'
+
+    def get_queryset(self):
+        """Return the last five published devotions."""
+        return Devotion.objects.order_by('-pub_date')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(DevotionView, self).get_context_data(**kwargs)
+        devotion_list = self.get_queryset()
+        verse_list = []
+        book_list = []
+        for devotion in devotion_list:
+            verse_id = int(devotion.verse_id)
+            version_id = int(devotion.version_id)
+
+            if version_id == 1:
+                model = TAsv
+            elif version_id == 2:
+                model = TBbe
+            elif version_id == 3:
+                model = TDby
+            elif version_id == 4:
+                model = TKjv
+            elif version_id == 5:
+                model = TWbt
+            elif version_id == 6:
+                model = TWeb
+            elif version_id == 7:
+                model = TYlt
+            else:
+                raise Http404("Bible version does not exist")
+
+            verse = get_object_or_404(model, pk=verse_id)
+            book = get_object_or_404(KeyEnglish, pk=int(verse.b))
+            verse_list.append(verse)
+            book_list.append(book)
+        context['zipped_data'] = zip(self.get_queryset(), verse_list, book_list)
         return context
