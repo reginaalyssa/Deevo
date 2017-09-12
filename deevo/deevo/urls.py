2cea61a47f842564b0b5360f8015aa57b7cac3a4
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-13 04:56:42 +0800

    Add style for homepage

--- deevo/deevo/urls.py
+++ deevo/deevo/urls.py
@@ -23,11 +23,11 @@ from . import views
 urlpatterns = [
     url(r'^devotions/', include('devotions.urls')),
     url(r'^bible/', include('bible.urls')),
+    url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.deevo_login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^profile/$', views.view_profile, name='profile'),
     url(r'^password/$', views.change_password, name='change_password'),
