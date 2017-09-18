0c603a993d865bbdb239c9d0844420ada7e0e6af
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-18 16:40:58 +0800

    Add initial header design to About page

---
+++ deevo/static/backstretch-images.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$("#home").backstretch([
+		"../static/images/about/1.jpg",
+		"../static/images/about/2.jpg",
+		"../static/images/about/3.jpg",
+		"../static/images/about/4.jpg",
+		"../static/images/about/5.jpg",
+		"../static/images/about/6.jpg",
+		], {
+		fade: 3000,
+		duration: 5000
+		}
+	);
+});
