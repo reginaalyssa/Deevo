ccca7bf3c64de8310833d195f4c314b84fb7774a
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-18 10:50:38 +0800

    Convert chapter in Bible views.py to class-based view

--- deevo/bible/views.py
+++ deevo/bible/views.py
@@ -58,26 +58,29 @@ class BookListView(ListView):
         context = super(BookListView, self).get_context_data(**kwargs)
         version_id = self.kwargs["version_id"]
         version = get_object_or_404(BibleVersionKey, abbreviation=version_id.upper())
-        form = VersionForm(self.request.POST or None, initial={"version": version.id})
+        context['form'] = VersionForm(self.request.POST or None, initial={"version": version.id})
         context['version'] = version_id
-        context['form'] = form
         return context
 
-def chapter(request, version_id, book_id, chapter_id):
-    model = get_version_model_from_id(version_id)
-    version = get_object_or_404(BibleVersionKey, abbreviation=version_id.upper())
-    verse_list = get_list_or_404(model, b=book_id, c=chapter_id)
-    book = get_object_or_404(KeyEnglish, pk=book_id)
-    form = VersionForm(request.POST or None, initial={"version": version.id})
-    context = {
-        'version': version,
-        'version_link': version_id,
-        'verse_list': verse_list,
-        'book': book,
-        'chapter': chapter_id,
-        'form': form,
-    }
-    return render(request, 'bible/chapter.html', context)
+class ChapterListView(ListView):
+    template_name = 'bible/chapter.html'
+    context_object_name = 'verse_list'
+
+    def get_queryset(self):
+        version_id = self.kwargs.get("version_id")
+        model = get_version_model_from_id(version_id)
+        queryset = get_list_or_404(model, b=self.kwargs.get("book_id"), c=self.kwargs.get("chapter_id"))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ChapterListView, self).get_context_data(**kwargs)
+        version = get_object_or_404(BibleVersionKey, abbreviation=self.kwargs["version_id"].upper())
+        context['form'] = VersionForm(self.request.POST or None, initial={"version": version.id})
+        context['version'] = version
+        context['book'] = get_object_or_404(KeyEnglish, pk=self.kwargs["book_id"])
+        context['chapter'] = self.kwargs["chapter_id"]
+        context['version_link'] = self.kwargs["version_id"]
+        return context
 
 def verse(request, version_id, book_id, chapter_id, verse_id):
     model = get_version_model_from_id(version_id)
