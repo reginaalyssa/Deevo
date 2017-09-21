a93790cddd98250587fcda7f484c16e5708235cf
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-09-21 14:07:17 +0800

    Refactor main container and header h1 in login and signup to base template and stylesheet

--- deevo/deevo/forms.py
+++ deevo/deevo/forms.py
@@ -10,6 +10,9 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm, self).__init__(*args, **kargs)
         del self.fields['password2']
 
+        for fieldname in ['username', 'password1']:
+            self.fields[fieldname].help_text = None
+
     email = forms.EmailField(max_length=254, help_text='We take privacy very seriously and promise not to '
                                                        'share your email address with anyone.')
     helper = FormHelper()
